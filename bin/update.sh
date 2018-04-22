f1cbd033da84d939cf4644366b7d27627add601a
Tim Bernhard <tim@bernhard-webstudio.ch>
2018-04-22 13:51:54 +0200

    add script to run on deployment server

---
+++ bin/update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Copyright (c) 2018 Tim Bernhard, KWI
+# Pulls from remote, installs dependencies and executes the update command.
+
+set -e
+cd "$(dirname $0)/.."
+
+git pull
+composer install --no-dev --optimize-autoloader
+php bin/console cache:clear --env=prod --no-debug
+yarn run enore production
