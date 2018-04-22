389a56ec4f99a1286599d1fe4bf9e2e43004d7a6
Tim Bernhard <tim@bernhard-webstudio.ch>
2018-04-22 14:00:20 +0200

    add futher necessary objects to have reset permissions

--- bin/fix-permissions.sh
+++ bin/fix-permissions.sh
@@ -5,5 +5,5 @@ cd "$(dirname $0)/.."
 chown -R root:www-data .
 chmod -R 0700 .
 chmod 0750 .
-chmod -R 0750 app web vendor src var templates node_modules
+chmod -R 0750 config public vendor src var templates translations node_modules assets .env
 chmod -R 0770 var/cache var/logs
